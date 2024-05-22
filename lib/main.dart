5cf765052ced02a62e085cf758857913a7fa108b
tshabalalaaj <tshabalalaaj@gmail.com>
2024-05-22 16:40:19 +0000

    functions

--- lib/main.dart
+++ lib/main.dart
@@ -29,21 +29,46 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List <Widget> scoreKeeper = [
+    const Icon(
+       Icons.check,
+        color: Colors.green,
+      ),
+      const Icon(
+        Icons.close,
+        color: Colors.red,
+      ),
+  ];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int questionNumber = 0;
+
+  int questionCount(){
+    return questionNumber++;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 25.0,
                   color: Colors.white,
                 ),
@@ -57,6 +82,9 @@ class _QuizPageState extends State<QuizPage> {
             child: TextButton(
               style: TextButton.styleFrom(
                 backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
               ),
               child: const Text(
                 'True',
@@ -66,7 +94,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  questionCount();
+                });
+                
               },
             ),
           ),
@@ -77,6 +108,9 @@ class _QuizPageState extends State<QuizPage> {
             child: TextButton(
               style: TextButton.styleFrom(
                 backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
               ),
               child: const Text(
                 'False',
@@ -86,12 +120,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  questionCount();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
